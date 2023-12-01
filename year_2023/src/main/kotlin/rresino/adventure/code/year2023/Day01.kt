0bc9e57c5a579c6a3fdaa7f1fbf1268a38629a56
Rubén Resino <rresino@gmail.com>
2023-12-01 18:56:10 +0100

    :rocket: Year 2023 - Day 01 - step 2

--- year_2023/src/main/kotlin/rresino/adventure/code/year2023/Day01.kt
+++ year_2023/src/main/kotlin/rresino/adventure/code/year2023/Day01.kt
@@ -2,20 +2,54 @@ package rresino.adventure.code.year2023
 
 object Day01 {
 
-    fun run(useDemo: Boolean): Int {
+    fun runStep1(useDemo: Boolean): Int {
         val lines = Utils.readCleanInput(if (useDemo) "day01.demo" else "day01")
-
-        return lines.map { line ->
-                val onlyDigits = line.filter { it.isDigit() }
-                "${onlyDigits.first()}${onlyDigits.last()}".toInt()
-            }.sum()
+        return getCalibrationNumbers(lines)
     }
 
+    fun runStep2(useDemo: Boolean): Int {
+        val lines = Utils.readCleanInput(if (useDemo) "day01.demo2" else "day01")
+        val cleanLines = lines.map { replaceTextDigitsToDigits(it) }
+        return getCalibrationNumbers(cleanLines)
+    }
+
+    private fun getCalibrationNumbers(lines: List<String>): Int {
+        return lines.sumOf { line ->
+            val onlyDigits = line.filter { it.isDigit() }
+            "${onlyDigits.first()}${onlyDigits.last()}".toInt()
+        }
+    }
+
+    private fun replaceTextDigitsToDigits(str: String): String {
+        return str.foldIndexed("") {index, acc, c ->
+            val partialStr = str.substring(index)
+            when {
+                c.isDigit() -> acc + c
+                partialStr.startsWith("one") -> acc + "1"
+                partialStr.startsWith("two") -> acc + "2"
+                partialStr.startsWith("three") -> acc + "3"
+                partialStr.startsWith("four") -> acc + "4"
+                partialStr.startsWith("five") -> acc + "5"
+                partialStr.startsWith("six") -> acc + "6"
+                partialStr.startsWith("seven") -> acc + "7"
+                partialStr.startsWith("eight") -> acc + "8"
+                partialStr.startsWith("nine") -> acc + "9"
+                else -> acc
+            }
+        }
+    }
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Day 01 - Demo ${run(true)}")
-        println("Day 01 - ${run(false)}")
+        println()
+        println("Step 1")
+        println("Day 01 - Demo ${runStep1(true)}")
+        println("Day 01 - ${runStep1(false)}")
+        println()
+        println("Step 2")
+        println("Day 01 - Demo ${runStep2(true)}")
+        println("Day 01 - ${runStep2(false)}")
+        println()
     }
 
 }
